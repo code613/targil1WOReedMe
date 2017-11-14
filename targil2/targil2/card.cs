0369a60065aa3b10ded9523cc38336206d59d4b7
zeevkaminsky <33220296+zeevkaminsky@users.noreply.github.com>
2017-11-14 13:10:15 +0200

    know you are wuth me

--- targil2/targil2/card.cs
+++ targil2/targil2/card.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace targil2
 {
+    enum Royal { jack = 11, queen, king, ace };
+    enum E_Color { red, black };
     class card
     {
+        private E_Color color;
+        private int number;
+        //#region
+        internal E_Color Color
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                if (value < 2 || value > 14)
+                {
+                    Console.WriteLine("ERROR");
+                    return;
+                }
+                number = value;
+            }
+        }
+
+        public string cardName()
+        {
+
+            if (number < 11)
+                return number.ToString();
+            else
+            {
+                Royal c = (Royal)number;
+                return c.ToString();
+            }
+
+        }
     }
 }
+                
+ 
+        
+
+           
+
